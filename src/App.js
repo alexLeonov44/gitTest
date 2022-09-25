86288521923c2501163922384347e10e551b168d
Alexander <leonovfx.91@gmail.com>
2022-09-25 14:10:10 +0300

    this commit from testing branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
+  console.log('this commit from testing branch')
   console.log('third test commit')
   console.log('second test commit')
   return <>
