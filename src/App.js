24d5f6f0fa09364315359576ce57fa9b5a6c4a34
Alexander <leonovfx.91@gmail.com>
2022-09-25 13:51:06 +0300

    third test commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
+  console.log('third test commit')
   console.log('second test commit')
   return <>
   <Counter/>
