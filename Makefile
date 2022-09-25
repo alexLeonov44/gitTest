c9a1b87bea5b315ceff2d4b11f3f06a4309d5fe6
Alexander <leonovfx.91@gmail.com>
2022-09-25 13:56:33 +0300

    in this commit i created new testing branch

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 log:
 	git log --pretty=oneline
+logd:
+	git log --oneline --decorate
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
+  console.log('in this commit i created new testing branch')
   console.log('third test commit')
   console.log('second test commit')
   return <>
