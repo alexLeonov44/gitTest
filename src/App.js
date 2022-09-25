8054301f571dcb417b05e87fadd2896d9d92700d
Alexander <leonovfx.91@gmail.com>
2022-09-25 14:19:03 +0300

    next commit from master branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
+  console.log('next commit from master branch')
   console.log('in this commit i created new testing branch')
   console.log('third test commit')
   console.log('second test commit')
