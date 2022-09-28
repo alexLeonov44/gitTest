07c3b326c9ac9e2df30e96ca33994184d0c16384
Alexander <leonovfx.91@gmail.com>
2022-09-28 22:38:47 +0300

    i just created second feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Counter } from './components/Counter';
 const App = () => {
  console.log('my master features')
  console.log('i just created new feature')
+ console.log('i just created second feature')
   return <>
   <Counter/>
   </>;
