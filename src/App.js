04a60dc5f239a4c1bef183d4b191f114178c9045
Alexander <leonovfx.91@gmail.com>
2022-09-29 20:44:49 +0300

    testbr 2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
- console.log('my master features')
- console.log('i just created new feature')
- console.log('i just created second feature')
+ console.log('testbr 2 ')
+
   return <>
   <Counter/>
   </>;
