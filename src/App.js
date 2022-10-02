ba611569a8bfa83ee2cf6b58fee3e4301ed98fb2
Alexander <leonovfx.91@gmail.com>
2022-10-02 10:34:59 +0300

    some changes in new branch else

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
-  console.log("some changes in new branch")
+  console.log("some changes in new branch else")
  console.log('total changes in master')
   return <>
   <Counter/>
