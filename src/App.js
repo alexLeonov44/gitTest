8e01dcda51a2a6afeb6a29dd80ca7a12d829b88c
Alexander <leonovfx.91@gmail.com>
2022-10-02 10:34:21 +0300

    some changes in master else

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
-  console.log('some changes in master')
+  console.log('some changes in master else')
  console.log('total changes in master')
   return <>
   <Counter/>
