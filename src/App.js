7b378a2ea206c5ef74686f4aa87ad4a8feebebb7
Alexander <leonovfx.91@gmail.com>
2022-10-02 09:43:34 +0300

    first sur extra content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './components/Counter';
 
 const App = () => {
+  console.log('first sur extra content')
  console.log('first sur')
  console.log('first sur some code')
   return <>
